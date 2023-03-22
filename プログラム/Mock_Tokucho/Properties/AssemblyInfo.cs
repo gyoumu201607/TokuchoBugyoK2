e8fb82ce39101ec8b9e7896524972334e7773108
Toma <toma@vips.co.jp>
2023-03-22 14:31:05 +0900

    本番用に、プロジェクトファイルとアセンブリインフォを更新

--- プログラム/Mock_Tokucho/Properties/AssemblyInfo.cs
+++ プログラム/Mock_Tokucho/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
-[assembly: AssemblyTitle("TokuchoBugyoK2_ST")]
+[assembly: AssemblyTitle("TokuchoBugyoK2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Construction Research Institute")]
-[assembly: AssemblyProduct("TokuchoBugyoK2_ST")]
+[assembly: AssemblyProduct("TokuchoBugyoK2")]
 [assembly: AssemblyCopyright("Copyright © Construction Research Institute 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
