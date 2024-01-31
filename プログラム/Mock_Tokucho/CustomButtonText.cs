dce76fafbb609ffb638f1e9e42c5a26f2efb2519
sato-h <sato.h@vips.co.jp>
2024-01-31 17:01:00 +0900

    No1658対応改修

--- プログラム/Mock_Tokucho/CustomButtonText.cs
+++ プログラム/Mock_Tokucho/CustomButtonText.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace TokuchoBugyoK2
 {
-    class CustomButtonText
+    public class CustomButtonText
     {
+        public string OK { get; set; }
+        public string Cancel { get; set; }
+        public string Abort { get; set; }
+        public string Retry { get; set; }
+        public string Ignore { get; set; }
+        public string Yes { get; set; }
+        public string No { get; set; }
     }
 }
